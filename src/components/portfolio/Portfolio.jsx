a934fe389212159de7895e5b6002f7db47e9c3a4
SokhibjonDev <110424000+SokhibjonDev@users.noreply.github.com>
2022-08-06 12:45:13 +0500

    feat : link _blank to portfolios

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,33 +1,36 @@
 import './Portfolio.css';
 
 function Portfolio() {
+    const openInNewTab = url => {
+        window.open(url, '_blank', 'noopener,noreferrer')
+    }
     return (
         <>
             <div className="title"><h1>Portfolio</h1></div>
             <div className="Portfolios">
                 <div className="Portfolio">
-                    <div className="portfolioInfo"><p><i className='uil uil-eye'></i> Live Demo</p><img src="/images/kombo.png" alt="" /></div>
-                    <div className="portfolioInfo"><p><i className='uil uil-eye'></i> Live Demo</p><img src="/images/travel.png" alt="" /></div>
-                    <div className="portfolioInfo"><p><i className='uil uil-eye'></i> Live Demo</p><img src="/images/jw.png" alt="" /></div>
-                    <div className="portfolioInfo"><p><i className='uil uil-eye'></i> Live Demo</p><img src="/images/profman.png" alt="" /></div>
+                    <div className="portfolioInfo"><a rel='noopener noreferrer' target='_blank' href="#!"><i className='uil uil-eye'></i> Live Demo</a><img src="/images/kombo.png" alt="" /></div>
+                    <div className="portfolioInfo"><a rel='noopener noreferrer' target='_blank' href="#!"><i className='uil uil-eye'></i> Live Demo</a><img src="/images/travel.png" alt="" /></div>
+                    <div className="portfolioInfo"><a rel='noopener noreferrer' target='_blank' href="https://www.jw.org/ru/"><i className='uil uil-eye'></i> Live Demo</a><img src="/images/jw.png" alt="" /></div>
+                    <div className="portfolioInfo"><a rel='noopener noreferrer' target='_blank' href="#!"><i className='uil uil-eye'></i> Live Demo</a><img src="/images/profman.png" alt="" /></div>
                 </div>
                 <div className="Portfolio">
-                    <div className="portfolioInfo"><p><i className='uil uil-eye'></i>Live Demo</p><img src="/images/iknow.png" alt="" /></div>
-                    <div className="portfolioInfo"><p><i className='uil uil-eye'></i>Live Demo</p><img src="/images/panteon.png" alt="" /></div>
-                    <div className="portfolioInfo"><p><i className='uil uil-eye'></i>Live Demo</p><img src="/images/mypro.png" alt="" /></div>
-                    <div className="portfolioInfo"><p><i className='uil uil-eye'></i>Live Demo</p><img src="/images/title_build.png" alt="" /></div>
+                    <div className="portfolioInfo"><a rel='noopener noreferrer' target='_blank' href="#!"><i className='uil uil-eye'></i>Live Demo</a><img src="/images/iknow.png" alt="" /></div>
+                    <div className="portfolioInfo"><a rel='noopener noreferrer' target='_blank' href="#!"><i className='uil uil-eye'></i>Live Demo</a><img src="/images/panteon.png" alt="" /></div>
+                    <div className="portfolioInfo"><a rel='noopener noreferrer' target='_blank' href="#!"><i className='uil uil-eye'></i>Live Demo</a><img src="/images/mypro.png" alt="" /></div>
+                    <div className="portfolioInfo"><a rel='noopener noreferrer' target='_blank' href="#!"><i className='uil uil-eye'></i>Live Demo</a><img src="/images/title_build.png" alt="" /></div>
                 </div>
                 <div className="Portfolio">
-                    <div className="portfolioInfo"><p><i className='uil uil-eye'></i>Live Demo</p><img src="/images/title_build.png" alt="" /></div>
-                    <div className="portfolioInfo"><p><i className='uil uil-eye'></i>Live Demo</p><img src="/images/porten.png" alt="" /></div>
-                    <div className="portfolioInfo"><p><i className='uil uil-eye'></i>Live Demo</p><img src="/images/north.png" alt="" /></div>
-                    <div className="portfolioInfo"><p><i className='uil uil-eye'></i>Live Demo</p><img src="/images/kombo.png" alt="" /></div>
+                    <div className="portfolioInfo"><a rel='noopener noreferrer' target='_blank' href='#!'><i className='uil uil-eye'></i>Live Demo</a><img src="/images/title_build.png" alt="" /></div>
+                    <div className="portfolioInfo"><a rel='noopener noreferrer' target='_blank' href='#!'><i className='uil uil-eye'></i>Live Demo</a><img src="/images/porten.png" alt="" /></div>
+                    <div className="portfolioInfo"><a rel='noopener noreferrer' target='_blank' href='#!'><i className='uil uil-eye'></i>Live Demo</a><img src="/images/north.png" alt="" /></div>
+                    <div className="portfolioInfo"><a rel='noopener noreferrer' target='_blank' href='#!'><i className='uil uil-eye'></i>Live Demo</a><img src="/images/kombo.png" alt="" /></div>
                 </div>
                 <div className="Portfolio">
-                    <div className="portfolioInfo"><p><i className='uil uil-eye'></i>Live Demo</p><img src="/images/mypro.png" alt="" /></div>
-                    <div className="portfolioInfo"><p><i className='uil uil-eye'></i>Live Demo</p><img src="/images/travel.png" alt="" /></div>
-                    <div className="portfolioInfo"><p><i className='uil uil-eye'></i>Live Demo</p><img src="/images/north.png" alt="" /></div>
-                    <div className="portfolioInfo"><p><i className='uil uil-eye'></i>Live Demo</p><img src="/images/kombo.png" alt="" /></div>
+                    <div className="portfolioInfo"><a rel='noopener noreferrer' target='_blank' href='#!'><i className='uil uil-eye'></i>Live Demo</a><img src="/images/mypro.png" alt="" /></div>
+                    <div className="portfolioInfo"><a rel='noopener noreferrer' target='_blank' href='#!'><i className='uil uil-eye'></i>Live Demo</a><img src="/images/travel.png" alt="" /></div>
+                    <div className="portfolioInfo"><a rel='noopener noreferrer' target='_blank' href='#!'><i className='uil uil-eye'></i>Live Demo</a><img src="/images/north.png" alt="" /></div>
+                    <div className="portfolioInfo"><a rel='noopener noreferrer' target='_blank' href='#!'><i className='uil uil-eye'></i>Live Demo</a><img src="/images/kombo.png" alt="" /></div>
                 </div>
             </div>
         </>
