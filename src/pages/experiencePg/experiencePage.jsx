887bf3ff2797bf00fcc83fdd58281124da1d5e03
SokhibjonDev <110424000+SokhibjonDev@users.noreply.github.com>
2022-08-07 19:21:54 +0500

    feat : push experience update

--- src/pages/experiencePg/experiencePage.jsx
+++ src/pages/experiencePg/experiencePage.jsx
@@ -85,6 +85,168 @@ const experiencePage = () => {
           </div>
         </div>
       </div>
+      <h1 className="titlecha">Front End Development</h1>
+      <p className="params">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laboriosam mollitia consequuntur cum voluptates iure dolores excepturi alias explicabo hic ipsam qui, distinctio ratione est. Blanditiis cupiditate distinctio repellendus suscipit magni voluptatibus corporis delectus magnam amet reiciendis labore aspernatur nostrum adipisci illo commodi, sint pariatur velit. Sunt porro est voluptatibus earum saepe fugiat ex dolore deleniti adipisci ipsa excepturi modi dolores, repellendus suscipit ab reiciendis? Eum, distinctio quam debitis possimus, cumque modi optio quibusdam eligendi, autem cum quos. Itaque, sequi quaerat enim totam voluptatem consequuntur officiis eveniet eius atque blanditiis, repellat magni deserunt impedit amet pariatur fugiat provident in, quisquam asperiores.</p>
+      <div className="ExpeienceCards">
+        <div className="card">
+          <div className="percent">
+            <div className="dot"></div>
+            <svg>
+              <circle cx={70} cy={70} r={70}></circle>
+              <circle cx={70} cy={70} r={70}></circle>
+            </svg>
+            <div className="number">
+              <h2>
+                85 <span>%</span>
+              </h2>
+              <p>HTML</p>
+            </div>
+          </div>
+        </div>
+        <div className="card">
+          <div className="percent">
+            <div className="dot"></div>
+            <svg>
+              <circle cx={70} cy={70} r={70}></circle>
+              <circle cx={70} cy={70} r={70}></circle>
+            </svg>
+            <div className="number">
+              <h2>
+                85 <span>%</span>
+              </h2>
+              <p>HTML</p>
+            </div>
+          </div>
+        </div>
+        <div className="card">
+          <div className="percent">
+            <div className="dot"></div>
+            <svg>
+              <circle cx={70} cy={70} r={70}></circle>
+              <circle cx={70} cy={70} r={70}></circle>
+            </svg>
+            <div className="number">
+              <h2>
+                85 <span>%</span>
+              </h2>
+              <p>HTML</p>
+            </div>
+          </div>
+        </div>
+        <div className="card">
+          <div className="percent">
+            <div className="dot"></div>
+            <svg>
+              <circle cx={70} cy={70} r={70}></circle>
+              <circle cx={70} cy={70} r={70}></circle>
+            </svg>
+            <div className="number">
+              <h2>
+                85 <span>%</span>
+              </h2>
+              <p>HTML</p>
+            </div>
+          </div>
+        </div>
+        <div className="card">
+          <div className="percent">
+            <div className="dot"></div>
+            <svg>
+              <circle cx={70} cy={70} r={70}></circle>
+              <circle cx={70} cy={70} r={70}></circle>
+            </svg>
+            <div className="number">
+              <h2>
+                85 <span>%</span>
+              </h2>
+              <p>HTML</p>
+            </div>
+            
+          </div>
+        </div>
+      </div>
+      <h1 className="titlecha">Back End Development</h1>
+      <p className="params">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laboriosam mollitia consequuntur cum voluptates iure dolores excepturi alias explicabo hic ipsam qui, distinctio ratione est. Blanditiis cupiditate distinctio repellendus suscipit magni voluptatibus corporis delectus magnam amet reiciendis labore aspernatur nostrum adipisci illo commodi, sint pariatur velit. Sunt porro est voluptatibus earum saepe fugiat ex dolore deleniti adipisci ipsa excepturi modi dolores, repellendus suscipit ab reiciendis? Eum, distinctio quam debitis possimus, cumque modi optio quibusdam eligendi, autem cum quos. Itaque, sequi quaerat enim totam voluptatem consequuntur officiis eveniet eius atque blanditiis, repellat magni deserunt impedit amet pariatur fugiat provident in, quisquam asperiores.</p>
+      <div className="ExpeienceCards">
+        <div className="card">
+          <div className="percent">
+            <div className="dot"></div>
+            <svg>
+              <circle cx={70} cy={70} r={70}></circle>
+              <circle cx={70} cy={70} r={70}></circle>
+            </svg>
+            <div className="number">
+              <h2>
+                85 <span>%</span>
+              </h2>
+              <p>HTML</p>
+            </div>
+          </div>
+        </div>
+        <div className="card">
+          <div className="percent">
+            <div className="dot"></div>
+            <svg>
+              <circle cx={70} cy={70} r={70}></circle>
+              <circle cx={70} cy={70} r={70}></circle>
+            </svg>
+            <div className="number">
+              <h2>
+                85 <span>%</span>
+              </h2>
+              <p>HTML</p>
+            </div>
+          </div>
+        </div>
+        <div className="card">
+          <div className="percent">
+            <div className="dot"></div>
+            <svg>
+              <circle cx={70} cy={70} r={70}></circle>
+              <circle cx={70} cy={70} r={70}></circle>
+            </svg>
+            <div className="number">
+              <h2>
+                85 <span>%</span>
+              </h2>
+              <p>HTML</p>
+            </div>
+          </div>
+        </div>
+        <div className="card">
+          <div className="percent">
+            <div className="dot"></div>
+            <svg>
+              <circle cx={70} cy={70} r={70}></circle>
+              <circle cx={70} cy={70} r={70}></circle>
+            </svg>
+            <div className="number">
+              <h2>
+                85 <span>%</span>
+              </h2>
+              <p>HTML</p>
+            </div>
+          </div>
+        </div>
+        <div className="card">
+          <div className="percent">
+            <div className="dot"></div>
+            <svg>
+              <circle cx={70} cy={70} r={70}></circle>
+              <circle cx={70} cy={70} r={70}></circle>
+            </svg>
+            <div className="number">
+              <h2>
+                85 <span>%</span>
+              </h2>
+              <p>HTML</p>
+            </div>
+            
+          </div>
+        </div>
+      </div>
+      <h1 className="titlecha">Bootcamp Development</h1>
+      <p className="params">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laboriosam mollitia consequuntur cum voluptates iure dolores excepturi alias explicabo hic ipsam qui, distinctio ratione est. Blanditiis cupiditate distinctio repellendus suscipit magni voluptatibus corporis delectus magnam amet reiciendis labore aspernatur nostrum adipisci illo commodi, sint pariatur velit. Sunt porro est voluptatibus earum saepe fugiat ex dolore deleniti adipisci ipsa excepturi modi dolores, repellendus suscipit ab reiciendis? Eum, distinctio quam debitis possimus, cumque modi optio quibusdam eligendi, autem cum quos. Itaque, sequi quaerat enim totam voluptatem consequuntur officiis eveniet eius atque blanditiis, repellat magni deserunt impedit amet pariatur fugiat provident in, quisquam asperiores.</p>
     </div>
   );
 };
